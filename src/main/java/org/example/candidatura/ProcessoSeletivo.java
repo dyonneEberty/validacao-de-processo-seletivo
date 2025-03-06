444674badca2a30039a8b08338dfc2cc8ad2aac9
dyonneEberty <bydyonne@gmail.com>
2025-03-06 09:08:42 -0300

    feat: Implementado metodo para analisar cadidato

--- src/main/java/org/example/candidatura/ProcessoSeletivo.java
+++ src/main/java/org/example/candidatura/ProcessoSeletivo.java
@@ -3,7 +3,20 @@ package org.example.candidatura;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
 
+        analisarCandidato(2000.0);
+        analisarCandidato(2500.0);
+        analisarCandidato(100.0);
     }
 
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
 
+        if(salarioBase > salarioPretendido)
+            System.out.println("Ligar para o candidato");
+        else if (salarioBase == salarioPretendido) {
+            System.out.println("Ligar para o candidato com contra proposta");
+        } else {
+            System.out.println("Aguardado resultado dos demais candidatos");
+        }
+    }
 }
